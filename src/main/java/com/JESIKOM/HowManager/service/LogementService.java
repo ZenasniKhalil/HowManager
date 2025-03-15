4852b6d5a9fca01b1b48dcf3344ab25cb211d456
steph-ctrl <stephanejeanbaptiste@yahoo.com>
2025-03-15 18:34:16 +0100

    commit for no reason

--- src/main/java/com/JESIKOM/HowManager/service/LogementService.java
+++ src/main/java/com/JESIKOM/HowManager/service/LogementService.java
@@ -48,7 +48,6 @@ public class LogementService implements ILogementService {
     }
 
 
-
     public List<Logement> getLogementByCapacite(int capacite) {
         return logementRepository.findLogementByCapacite(capacite);
     }
