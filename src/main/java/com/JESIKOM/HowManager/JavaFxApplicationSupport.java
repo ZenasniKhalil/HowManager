57d51479cae10c03de7d33b9be32b10c354a6665
jeremie.pennec <jeremie.pennec@universite-paris-saclay.fr>
2025-03-19 16:33:29 +0100

    Link entre JavaFx et Spring: Quand on lance HowManagerApplication -> lance javafx, quand on ferme l'interface : -> ferme Spring App.java renommée en JavaFxApplicationSupport.java

---
+++ src/main/java/com/JESIKOM/HowManager/JavaFxApplicationSupport.java
@@ -0,0 +1,45 @@
+package com.JESIKOM.HowManager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+
+public class JavaFxApplicationSupport extends Application {
+
+    private static ConfigurableApplicationContext context;
+
+    public static void setContext(ConfigurableApplicationContext ctx) {
+        context = ctx;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/main.fxml"));
+        loader.setControllerFactory(context::getBean);  // Injection Spring dans le FXML
+
+        Parent root = loader.load();
+        primaryStage.setTitle("How Manager");
+        primaryStage.setScene(new Scene(root, 950, 700));
+        primaryStage.show();
+
+        // Fermer Spring à la fermeture de la fenêtre
+        primaryStage.setOnCloseRequest(event -> context.close());
+    }
+
+    @Override
+    public void stop() {
+        if (context != null) {
+            context.close();
+        }
+    }
+}
